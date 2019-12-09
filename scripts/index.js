726b792fb7288278abc0f150f43e64d43ecc7307
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-09 14:58:44 -0500

    Adds small, medium, and large exercises and image paths for menu

---
+++ scripts/index.js
@@ -0,0 +1,74 @@
+function getCategories(obj) {
+    const arrayOfCategoryNames = Object.keys(obj);
+    return arrayOfCategoryNames;
+}
+
+function nameToListItem(categoryName) {
+    let liEl = document.createElement('li');
+    liEl.textContent = categoryName;
+    liEl.addEventListener('click', getMenuObjsOnClick);
+    return liEl;
+}
+
+function categoriesToListItem(arrayOfCategoryNames) {
+    const listItems = arrayOfCategoryNames.map(nameToListItem);
+    return listItems;
+}
+
+const categories = getCategories(menu);
+const listItems = categoriesToListItem(categories);
+const jsMenu = document.querySelector('.js-menu');
+
+function appendLiTojsMenu(li) {
+    jsMenu.appendChild(li);
+}
+
+listItems.map(appendLiTojsMenu);
+
+function getMenuObjsOnClick(event) {
+    jsMainContent.textContent = "";
+    let name = event.target.textContent;
+    let itemObjs = categoryToItemObjs(name);
+    // debugger;
+    let itemCards = itemObjs.map(itemToCard);
+    itemCards.map(appendCardToMainContent);
+}
+
+function itemObjFromName(name) {
+
+}
+
+function getName(obj) {
+    return obj.name;
+}
+function categoryToItemObjs(menuName) {
+    const items = menu[menuName];
+    // const itemNames = items.map(getName);
+    return items;
+}
+
+const liItems = document.querySelectorAll('li');
+
+// console.log(categoryToItems('breakfast'));
+
+function itemToCard(itemObj) {
+    // debugger;
+    let div = document.createElement('div');
+    div.className = "card";
+    // jsMenu.appendChild(div);
+    let name = document.createElement('h2');
+    name.textContent = itemObj.name;
+    div.appendChild(name);
+    let price = document.createElement('h3');
+    price.textContent = itemObj.price;
+    div.appendChild(price);
+    return div;
+}
+
+// itemToCard(menu.breakfast[0]);
+const jsMainContent = document.querySelector('.js-main-content');
+function appendCardToMainContent(cardEl) {
+    jsMainContent.appendChild(cardEl);
+}
+// appendCardToMainContent(itemToCard(menu.breakfast[0]));
+
--- scripts/menu.js
+++ scripts/menu.js
@@ -7,7 +7,7 @@ const menu = {
             isVegan: false,
             description: "Two biscuits and some gravy!",
             price: 4,
-            photo: ""
+            photo: "images/jodie-morgan-PAa_MJztyUY-unsplash.jpg"
         },     
         {
             name: "Granola",
@@ -15,7 +15,7 @@ const menu = {
             isVegan: true,
             description: "Fruit, granola, and yogurt.",
             price: 3,
-            photo: ""
+            photo: "images/dan-counsell-efJu9SKIjLA-unsplash.jpg"
         },     
         {
             name: "Breakfast Tacos",
@@ -23,7 +23,7 @@ const menu = {
             isVegan: false,
             description: "Tacos. In your face!",
             price: 8,
-            photo: ""
+            photo: "images/tai-s-captures-JiRSy0GfqPA-unsplash.jpg"
         },     
         {
             name: "Pancakes",
@@ -31,7 +31,7 @@ const menu = {
             isVegan: false,
             description: "A stack of yum",
             price: 7,
-            photo: ""
+            photo: "images/elli-o-7hlOjB5VVb0-unsplash.jpg"
         },     
     ],
     lunch: [
@@ -41,7 +41,7 @@ const menu = {
             isVegan: false,
             description: "The taste of freedom",
             price: 9,
-            photo: ""
+            photo: "images/beef-blur-bread-bun-551991.jpg"
         },     
         {
             name: "Salad",
@@ -49,7 +49,7 @@ const menu = {
             isVegan: true,
             description: "The taste of vegetables",
             price: 7,
-            photo: ""
+            photo: "images/vegetable-salad-with-wheat-bread-on-the-side-1213710.jpg"
         },     
         {
             name: "BLT",
@@ -57,7 +57,7 @@ const menu = {
             isVegan: false,
             description: "It's like a salad, but it's a sandwich. And there's bacon.",
             price: 7,
-            photo: ""
+            photo: "images/eiliv-sonas-aceron-PlLvvTs-kxU-unsplash.jpg"
         },     
         {
             name: "Veggie Soup",
@@ -65,7 +65,7 @@ const menu = {
             isVegan: true,
             description: "Because it's cold outside",
             price: 0,
-            photo: ""
+            photo: "images/jeeray-tang-mrGYttsi4iU-unsplash.jpg"
         },     
     ],
     dinner: [
@@ -75,7 +75,7 @@ const menu = {
             isVegan: false,
             description: "So healthy!",
             price: 11,
-            photo: ""
+            photo: "images/davide-cantelli-jpkfc5_d-DI-unsplash.jpg"
         },     
         {
             name: "Steak",
@@ -83,7 +83,7 @@ const menu = {
             isVegan: false,
             description: "Because meat",
             price: 13,
-            photo: ""
+            photo: "images/eugene-Xk0jQPZseMk-unsplash.jpg"
         },     
         {
             name: "Veggie Kebabs",
@@ -91,7 +91,7 @@ const menu = {
             isVegan: true,
             description: "Food on sticks",
             price: 9,
-            photo: ""
+            photo: "images/ella-olsson-Pb9aFVR9-Bk-unsplash.jpg"
         },     
         {
             name: "Hummus Plate",
@@ -99,7 +99,7 @@ const menu = {
             isVegan: true,
             description: "It's like an appetizer, but bigger.",
             price: 8,
-            photo: ""
+            photo: "images/kyle-brinker-uAOQvpSI6MY-unsplash.jpg"
         },     
     ],
     desserts: [
@@ -109,7 +109,7 @@ const menu = {
             isVegan: false,
             description: "There's always room for ice cream",
             price: 4,
-            photo: ""
+            photo: "images/dovile-ramoskaite-iT4qcNMhYTQ-unsplash.jpg"
         },
         {
             name: "Cheesecake",
@@ -117,7 +117,7 @@ const menu = {
             isVegan: false,
             description: "There's always room for cheesecake",
             price: 5,
-            photo: ""
+            photo: "images/tina-guina-s8_7AqkzCWY-unsplash.jpg"
 
         },
         {
@@ -126,7 +126,7 @@ const menu = {
             isVegan: false,
             description: "There's always room for fancy cake",
             price: 6,
-            photo: ""
+            photo: "images/tanya-prodan-XTI0pEqQ8DI-unsplash.jpg"
 
         }
     ]
